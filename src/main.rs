e0f837be58228a8a5ae1eafa7e4d6e379064f4f7
BriceLucifer <2376671337@qq.com>
2024-11-09 02:02:00 +0800

    make a REPl envirment to show how it compress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::{io::stdin, process::exit};
 
-fn main() {
-    let word = "lossless".chars().collect::<Vec<char>>();
-    let (encoded_word, codes) = huffman::huffman_encoding(word);
 
-    println!("Word: lossless");
-    println!("Huffman code: {}", encoded_word);
-    println!("Conversion table: {:?}", codes);
+fn main() -> std::io::Result<()> {
+    let mut buffer = String::new();
+    loop {
+        stdin().read_line(&mut buffer)?;
+        if buffer.trim() == "exit".to_string() {
+            exit(0)
+        }
+        print!("Word: {}",&buffer);
+        let (encoded_word, codes) = huffman::huffman_encoding(buffer.trim().chars().collect());
+        println!("Huffman code: {}", encoded_word);
+        println!("Conversion table: {:?}", codes);
+        buffer.clear();
+    }
 }
